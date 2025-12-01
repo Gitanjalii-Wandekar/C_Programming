f0e348464db4b296dc9b954bfa7612e25889daba
Gitanjalii-Wandekar <wandekargitanjali@gmail.com>
2025-12-01 21:27:19 +0530

    Add files via upload

---
+++ Assignments/Assignment29/program1_3.c
@@ -0,0 +1,53 @@
+/*
+   a    b   c   d   e
+   1    3   5   7   9
+   a    b   c   d   e
+   1    3   5   7   9
+*/ 
+
+#include<stdio.h>
+void Pattern(int iRow , int iCol)
+{
+   int i = 0, j= 0;
+
+   for(i = 1; i <= iRow ; i++)
+   {
+      char ch ='\0';
+      if(i % 2 != 0)
+      {
+          
+          for(j = 1, ch = 'a'; j <= iCol ; j++, ch++)
+          {
+            printf("%c\t", ch);
+          }
+
+         }
+         else
+            {
+               int num = 1;
+               for(j = 1; j <= iCol ; j++)
+               {
+                  printf("%d\t", num);
+                  num += 2;
+               }
+      
+         
+            }
+       
+    printf("\n");
+   }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows:");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of colums:");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
--- Assignments/Assignment29/program1_5.c
+++ Assignments/Assignment29/program1_5.c
@@ -1,28 +1,26 @@
 /*
-   1    2   3   4
-   5    6   7   8
-   9    1   2   3
-   4    5   6   7
+   1  2  3  4  
+   2  3  4  5
+   3  4  5  6
+   4  5  6  7
 */ 
 
 #include<stdio.h>
 void Pattern(int iRow , int iCol)
 {
    int i = 0, j= 0;
-   int num = 1;
 
    for(i = 1; i <= iRow ; i++)
    {
-    for(j = 1; j <= iCol ; j++)
-    {
-       printf("%d\t", num);
-       num++;
-       if(num == 0)
-       {
-         num = 1;
-       }
-    }
-    printf("\n");
+      
+          int num = i;
+          for(j = 1; j <= iCol ; j++)
+          {
+            printf("%d\t", num);
+            num ++;
+          }
+          printf("\n");
+
    }
 }
 int main()
